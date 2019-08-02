bfef40ca541a7186e244f183b3ea205e3a666539
tonypan <tonypan@umich.edu>
2019-08-02 15:57:36 -0400

    adding camera localization

---
+++ CameraLocalizer.py
@@ -0,0 +1,80 @@
+import process_markers
+import numpy as np
+from math import cos, sin, pi, sqrt
+from threading import Thread
+
+rotation_matrix = lambda r: np.array([[cos(r), sin(r), 0], [-sin(r), cos(r), 0], [0, 0, 1]])
+
+
+# Reverse x axis
+# transposes in functions may need fixing
+class CameraLocalizer:
+
+    def __init__(self):
+        self.localizer = process_markers.Localizer()
+        self.world_position = np.zeros([6])
+        self.camera_cube_position = np.zeros([6])
+        self.cozmo_origin_location = np.zeros([3])
+        self.cozmo_origin_rotation = np.zeros([3])
+        self.change_of_bases_r_to_s = np.zeros([3, 3])
+        self.change_of_bases_s_to_r = np.zeros([3, 3])
+        self.ready = False
+
+    # start a thread for continuously processing markers
+    def start(self):
+        Thread(target=self._get_cam_pos, args=()).start()
+
+    # get camera coordinates and angle
+    def _get_cam_pos(self):
+        while True:
+            camera_cozmo_coord, camera_cozmo_ang, camera_cube_coord, camera_cube_ang = self.localizer.pose_from_camera()
+            if camera_cozmo_coord is not None:
+                self.world_position = np.array([camera_cozmo_coord[0], camera_cozmo_coord[1], camera_cozmo_coord[2],
+                                                camera_cozmo_ang[0], camera_cozmo_ang[1], camera_cozmo_ang[2]])
+                self.ready = True
+                # print("world pos: ", self.world_position)
+                self.camera_cube_position = np.array([camera_cube_coord[0], camera_cube_coord[1], camera_cube_coord[2],
+                                                camera_cube_ang[0], camera_cube_ang[1], camera_cube_ang[2]])
+
+    # pose: xyzrpy
+    # pass in: (cozmo pose to cozmo)
+    # updates: origin location and rotation
+    def recalculate_transform(self, cozmo_pose):
+        world_pose = self.world_position
+        self.cozmo_origin_rotation = world_pose[5] - cozmo_pose[5]
+        undo_cozmo_rotation = rotation_matrix(self.cozmo_origin_rotation)
+        self.change_of_bases_r_to_s = undo_cozmo_rotation
+        self.change_of_bases_s_to_r = np.linalg.inv(self.change_of_bases_r_to_s)
+
+        origin_to_cozmo = np.array([cozmo_pose[0], cozmo_pose[1], cozmo_pose[2]])
+        origin_to_cozmo = np.matmul(origin_to_cozmo, self.change_of_bases_r_to_s)
+        world_pos = np.array([world_pose[0], world_pose[1], world_pose[2]])
+        self.cozmo_origin_location = world_pos - origin_to_cozmo
+
+    def get_world_pose(self, object_pose):
+        object_pos = np.array([object_pose[0], object_pose[1], object_pose[2]])
+        object_pos = np.matmul(object_pos, self.change_of_bases_r_to_s)
+        world_to_obj = object_pos + self.cozmo_origin_location
+        obj_rot = object_pose[5] + self.cozmo_origin_rotation
+        print("cube in camera position (actual): ", self.camera_cube_position)
+        # fix yaw to -pi to pi
+        if obj_rot < -pi:
+            obj_rot += pi * 2
+        elif obj_rot > pi:
+            obj_rot -= pi * 2
+        return [world_to_obj[0], world_to_obj[1], 0, 0, 0, obj_rot]
+
+    # world_pose: xyzrpy
+    # pass in: (object position in world, cozmo position to cozmo)
+    def get_cozmo_pose(self, world_pose):
+        origin_to_object = np.array([world_pose[0] - self.cozmo_origin_location[0],
+                                     world_pose[1] - self.cozmo_origin_location[1],
+                                     world_pose[2] - self.cozmo_origin_location[2]])
+        origin_to_object = np.matmul(origin_to_object, self.change_of_bases_s_to_r)
+        cozmo_rot = world_pose[5] - self.cozmo_origin_rotation
+        # fix yaw to -pi to pi
+        if cozmo_rot < -pi:
+            cozmo_rot += pi * 2
+        elif cozmo_rot > pi:
+            cozmo_rot -= pi * 2
+        return [origin_to_object[0], origin_to_object[1], 0, 0, 0, cozmo_rot]
---
+++ process_markers.py
@@ -0,0 +1,147 @@
+import cv2 as cv
+import cv2.aruco as aruco
+import numpy as np
+import math
+from threading import Thread
+
+
+class Localizer:
+
+    DICTIONARYID = 0
+    MARKERLENGTH = 0.035  # in meters
+    XML_FILENAME = "calibration_data.xml"
+
+    def is_rotation_matrix(self, r):
+        rt = np.transpose(r)
+        should_be_identity = np.dot(rt, r)
+        I = np.identity(3, dtype=r.dtype)
+        n = np.linalg.norm(I - should_be_identity)
+        return n < 1e-6
+
+    # Calculates rotation matrix to euler angles x y z
+    def rotation_matrix_to_euler_angles(self, r):
+        assert (self.is_rotation_matrix(r))
+        sy = math.sqrt(r[0, 0] * r[0, 0] + r[1, 0] * r[1, 0])
+        singular = sy < 1e-6
+        if not singular:
+            x = math.atan2(r[2, 1], r[2, 2])
+            y = math.atan2(-r[2, 0], sy)
+            z = math.atan2(r[1, 0], r[0, 0])
+        else:
+            x = math.atan2(-r[1, 2], r[1, 1])
+            y = math.atan2(-r[2, 0], sy)
+            z = 0
+        return np.array([x, y, z])
+
+    class WebCam:
+        def __init__(self):
+            self.camera = cv.VideoCapture(1)
+            _, self.input_image = self.camera.read()
+
+        def start(self):
+            Thread(target=self._capture_image, args=()).start()
+
+        def _capture_image(self):
+            while True:
+                self.input_image = self.camera.read()[1]
+                cv.imshow("Image", self.input_image)
+                cv.waitKey(50)
+
+        def get_image(self):
+            return self.input_image
+
+    def __init__(self):
+        # self.cam = cv.VideoCapture(1)
+        self.cam = self.WebCam()
+        self.cam.start()
+        # read camera calibration data
+        fs = cv.FileStorage(self.XML_FILENAME, cv.FILE_STORAGE_READ)
+        if not fs.isOpened():
+            print("Invalid camera file")
+            exit(-1)
+        self.camMatrix = fs.getNode("camera_matrix").mat()
+        self.distCoeffs = fs.getNode("distortion_coefficients").mat()
+
+    def pose_from_camera(self):
+        input_image = self.cam.get_image()
+        if input_image is not None:
+            # undistort image
+            h, w = input_image.shape[:2]
+            newcameramtx, roi = cv.getOptimalNewCameraMatrix(self.camMatrix, self.distCoeffs, (w, h), 1, (w, h))
+            dst = cv.undistort(input_image, self.camMatrix, self.distCoeffs, None, newcameramtx)
+            # crop the image
+            x, y, w, h = roi
+            input_image = dst[y:y + h, x:x + w]
+
+            # detect markers from the input image
+            dictionary = aruco.Dictionary_get(self.DICTIONARYID)
+            parameters = aruco.DetectorParameters_create()
+            marker_corners, marker_ids, _ = aruco.detectMarkers(input_image, dictionary, parameters=parameters)
+
+            if marker_ids is not None:
+                # find index of center marker
+                index = 0
+                index1 = 0
+                for i in range(len(marker_ids)):
+                    if marker_ids[i] == 0:
+                        index = i
+                    elif marker_ids[i] == 1:
+                        index1 = i
+
+                # pose estimation
+                if len(marker_ids) > 1:
+                    rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(
+                        marker_corners, self.MARKERLENGTH, self.camMatrix, self.distCoeffs)
+
+                    # check if tvecs is none
+                    if tvecs is not None:
+                        # translate tvecs from relation to camera to a marker
+                        tvecs[index][0] -= tvecs[index1][0]
+
+                        # flip y axis
+                        tvecs[index][0][1] = -tvecs[index][0][1]
+
+                        # get angle from rotational matrix
+                        # convert rotational vector rvecs to rotational matrix
+                        # convert euler in relation to a marker
+                        rmat = np.empty([3, 3])
+                        cv.Rodrigues(rvecs[index][0], rmat)  # cozmo vector to matrix
+                        rmat_0 = np.empty([3, 3])
+                        cv.Rodrigues(rvecs[index1][0], rmat_0)  # base marker vector to matrix
+                        euler_angle = self.rotation_matrix_to_euler_angles(rmat)  # cozmo relative to camera
+                        euler_angle1 = self.rotation_matrix_to_euler_angles(rmat_0)  # base marker relative to camera
+                        euler_angle = euler_angle - euler_angle1  # cozmo relative to base marker
+
+                        # flip yaw
+                        euler_angle = -euler_angle
+
+                        # fix yaw to -pi to pi
+                        if euler_angle[2] < -math.pi:
+                            euler_angle[2] += math.pi * 2
+                        elif euler_angle[2] > math.pi:
+                            euler_angle[2] -= math.pi * 2
+
+                        # display annotations (IDs and pose)
+                        image_copy = input_image.copy()
+                        cv.putText(image_copy, "Cozmo Pose", (10, 20), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 0, 0))
+                        msg = "X(m): " + str(tvecs[index][0][0])
+                        cv.putText(image_copy, msg, (10, 45), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 0, 0))
+                        msg = "Y(m): " + str(tvecs[index][0][1])
+                        cv.putText(image_copy, msg, (10, 70), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 0, 0))
+                        msg = "Angle(rad): " + str(euler_angle[2])
+                        cv.putText(image_copy, msg, (10, 95), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 0, 0))
+                        aruco.drawDetectedMarkers(image_copy, marker_corners, marker_ids)
+                        cv.imshow("HD Pro Webcam C920", image_copy)
+                        cv.waitKey(100)
+
+                        # return the x, y, z coordinates of cozmo in relation to base marker,
+                        return tvecs[index][0], euler_angle
+                    else:
+                        return None, None
+                else:
+                    return None, None
+            else:
+                return None, None
+        else:
+            print("No camera found!")
+            return None, None
